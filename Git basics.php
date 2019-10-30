b375069f2d8650a3a64c86adcb5690103c5c1795
John <ngwenyaengraving@gmail.com>
2019-10-30 16:14:32 +0200

    New fles

--- Git basics.php
+++ Git basics.php
@@ -6,7 +6,13 @@ git status
 // commit changes
 git commit -am "Your message about what changed"
 
+// Push committed changes to github server
+git push
+
 // Pull in new changes from master
 git pull
+
+// add new files
+git add .
 
 
