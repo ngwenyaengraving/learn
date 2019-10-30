7874476ff2c2409e2dcb65519c5d5aca9918c220
Ben <bentaylor50@hotmail.com>
2019-10-30 15:26:09 +0200

    Adding snippet files

---
+++ Git basics.php
@@ -0,0 +1,12 @@
+<?php
+
+// check loacl status of repo
+git status
+
+// commit changes
+git commit -am "Your message about what changed"
+
+// Pull in new changes from master
+git pull
+
+
---
+++ PhpStrom basics.php
@@ -0,0 +1,17 @@
+<?php
+
+// format code to look pretty
+Ctrl + Alt + L
+
+// Find
+Ctrl + F
+
+// Find and replace
+Ctrl + R
+
+// Swap tabs
+Ctrl + Tab
+
+// Comment out code
+Ctrl + /
+
